7f7159dd8010c4b3ad9baf200b8db0be4d093fc1
Ulugbek Abdulazizov <69419457+Abdulazizovv@users.noreply.github.com>
2024-10-25 14:07:29 +0500

    Update loader.py

--- bot/loader.py
+++ bot/loader.py
@@ -5,9 +5,8 @@ from bot.data import config
 
 
 
-PROXY_URL = 'http://proxy.server:3128/'
 
-bot = Bot(token=config.BOT_TOKEN, proxy=PROXY_URL,parse_mode=types.ParseMode.HTML)
+bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
